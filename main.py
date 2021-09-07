6c00e35f8121d60570b0a1e51962bc33bdcee71c
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-07 18:41:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,14 +54,6 @@ intents=discord.Intents.all(),slashlog=True)
 bot.add_cog(misc(bot))
 sblpy.SBLCog(bot, env.get("sbl_tok"))
 
-@bot.slash(name='tests', cls=slash.models.SubCommand)
-async def testesss(ctx, u: discord.Member):
-  if u.author.id == bot.author_id:
-    await ctx.reply('hmm')
-
-@bot.slash(name='test', subcommands=[testesss])
-async def testttt(ctx):
-  await ctx.reply('hii')
 
 @bot.event
 async def on_guild_join(guild):
