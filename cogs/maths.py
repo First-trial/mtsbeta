50d8987f9c6e348760aed3defd94f5974a649f3a
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-11 21:29:43 +0530

    Update maths.py

--- cogs/maths.py
+++ cogs/maths.py
@@ -6,5 +6,9 @@ class MathCog(cog.SlashCog):
     self.bot = bot
 
   @cog.command()
-  async def add(self,ctx):
-    pass
+  async def add(self, ctx, firstnumber: float, secondnumber: float):
+    await ctx.reply(f'Your answer is {firstnumber + secondnumber}', ephemeral=True)
+    
+  @cog.command()
+  async def subtract(self, ctx, firstnumber: float, secondnumber: float):
+    await ctx.reply(f'Your answer is {firstnumber - secondnumber}', ephemeral=True)
