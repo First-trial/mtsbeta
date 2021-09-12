e0d1f4c10ff7df515f0d9fdcbc453cc55b50567a
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-12 11:32:49 +0530

    Update maths.py

--- cogs/maths.py
+++ cogs/maths.py
@@ -1,5 +1,10 @@
-from appcommands import cog
+from appcommands import cog, models
 import math
+c=[
+  models.Choice(name='red'),
+  models.Choice(name='green'),
+  models.Choice(name='blue')
+]
 
 class MathCog(cog.SlashCog):
   def __init__(self, bot):
@@ -12,3 +17,10 @@ class MathCog(cog.SlashCog):
   @cog.command()
   async def subtract(self, ctx, firstnumber: float, secondnumber: float):
     await ctx.reply(f'Your answer is {firstnumber - secondnumber}', ephemeral=True)
+
+  @cog.command()
+  async def choose(self, ctx, opt: models.Option(name="thing", choices=c)):
+    await ctx.reply(f'You chose opt')
+
+def setup(bot):
+  bot.add_cog(MathCog(bot))
