cfcf42400d239df8c561c9cf6c29a4718678345e
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-11-22 12:57:44 +0530

    Update models.py

--- core/models/models.py
+++ core/models/models.py
@@ -2,49 +2,12 @@ from tortoise import fields
 from tortoise.models import Model
 
 
-class reminders(Model):
-	id = fields.BigIntField(pk=True)
-	expires = fields.DatetimeField(index=True)
-	created = fields.DatetimeField(auto_now=True)
-	event = fields.TextField()
-	extra = fields.JSONField(default=dict)
-
-	@property
-	def kwargs(self):
-		return self.extra.get("kwargs", {})
-
-	@property
-	def args(self):
-		return self.extra.get("args", ())
-
 class balance(Model):
-  class Meta:
-    table = 'bal'
-
-  uid = fields.TextField()
+  uid = fields.TextField(unique=True)
   hand = fields.TextField()
   bank = fields.TextField()
 
-class inv(Model):
-  uid = fields.TextField()
-  item = fields.TextField()
-  count = fields.TextField()
-
-class dukaan(Model):
-  class Meta:
-    table = 'shop'
-
-  uid = fields.TextField()
-  item = fields.TextField()
-  cost = fields.TextField()
-
-class snipes:
-  channel_id = fields.BigIntField(pk=True)
-  author_id = fields.BigIntField()
-  content = fields.TextField()
-  delete_time = fields.DatetimeField(auto_now=True)
-
-  @property
-  def author(self):
-      return self.bot.get_user(self.author_id)
-
+class workers(Model):
+  uid = fields.TextField(unique=True)
+  work = fields.TextField()
+  
