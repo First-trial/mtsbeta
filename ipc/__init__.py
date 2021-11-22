083c8d383e3da30a0ccadf41dcd7188dd158d83e
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-11-22 13:23:40 +0530

    Delete ipc directory

--- ipc/__init__.py
+++
@@ -1,5 +0,0 @@
-from .server import Server
-from .client import Client
-from .errors import IpcError, JSONEncodeError
-
-__version__ = "0.1.5a"
--- ipc/errors.py
+++
@@ -1,7 +0,0 @@
-class IpcError(Exception):
-  """Base error"""
-  pass
-
-class JSONEncodeError(IpcError):
-  """An error raised when JSONDecode problem is there"""
-  pass
