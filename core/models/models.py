c1c1130de3a1b05161d302136f3d1e82f5f4c767
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-11-27 11:15:49 +0530

    Update models.py

--- core/models/models.py
+++ core/models/models.py
@@ -1,5 +1,7 @@
+from typing import Any
 from tortoise import fields
 from tortoise.models import Model
+from tortoise.fields.base import Field
 
 
 class balance(Model):
@@ -11,3 +13,16 @@ class workers(Model):
   uid = fields.BigIntField(unique=True)
   work = fields.TextField()
   
+class ListField(Field, list):
+  def __init__(self, field: Field, **kwargs) -> None:
+    super().__init__(**kwargs)
+    self.sub_field = field
+    self.SQL_TYPE = "%s[]" % field.SQL_TYPE
+
+
+  def to_python_value(self, value: Any) -> Any:
+    return list(map(self.sub_field.to_python_value, value))
+
+
+  def to_db_value(self, value: Any, instance: Any) -> Any:
+    return [self.sub_field.to_db_value(val, instance) for val in value]
