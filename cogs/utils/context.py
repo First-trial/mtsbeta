c75f511146676c33d62387dabb625dfd8bd88cfd
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-06 10:04:25 +0530

    Update context.py

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -2,113 +2,112 @@ from discord.ext import commands
 import asyncio
 import discord
 import io
-from cogs.utils.ms import Mesle
 
 class _ContextDBAcquire:
-	__slots__ = ('ctx', 'timeout')
+  __slots__ = ('ctx', 'timeout')
 
-	def __init__(self, ctx, timeout):
-		self.ctx = ctx
-		self.timeout = timeout
+  def __init__(self, ctx, timeout):
+    self.ctx = ctx
+    self.timeout = timeout
 
-	def __await__(self):
-		return self.ctx._acquire(self.timeout).__await__()
+  def __await__(self):
+    return self.ctx._acquire(self.timeout).__await__()
 
-	async def __aenter__(self):
-		await self.ctx._acquire(self.timeout)
-		return self.ctx.db
+  async def __aenter__(self):
+    await self.ctx._acquire(self.timeout)
+    return self.ctx.db
 
-	async def __aexit__(self, *args):
-		await self.ctx.release()
+  async def __aexit__(self, *args):
+    await self.ctx.release()
 
 
 class Context(commands.Context):
-	def __init__(self,*args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.pool = self.bot.pool
-		self._db = None
+  def __init__(self,*args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.pool = self.bot.pool
+    self._db = None
 
-	
-	async def entry_to_code(self, entries):
-		width = max(len(a) for a, b in entries)
-		output = ['```']
-		for name, entry in entries:
-			output.append(f'{name:<{width}}: {entry}')
-		output.append('```')
-		await self.send('\n'.join(output))
+  
+  async def entry_to_code(self, entries):
+    width = max(len(a) for a, b in entries)
+    output = ['```']
+    for name, entry in entries:
+      output.append(f'{name:<{width}}: {entry}')
+    output.append('```')
+    await self.send('\n'.join(output))
 
-	async def indented_entry_to_code(self, entries):
-		width = max(len(a) for a, b in entries)
-		output = ['```']
-		for name, entry in entries:
-			output.append(f'\u200b{name:>{width}}: {entry}')
-		output.append('```')
-		await self.send('\n'.join(output))
+  async def indented_entry_to_code(self, entries):
+    width = max(len(a) for a, b in entries)
+    output = ['```']
+    for name, entry in entries:
+      output.append(f'\u200b{name:>{width}}: {entry}')
+    output.append('```')
+    await self.send('\n'.join(output))
 
-	def __repr__(self):
-		# we need this for our cache key strategy
-		return '<Context>'
+  def __repr__(self):
+    # we need this for our cache key strategy
+    return '<Context>'
 
-	@property
-	def session(self):
-		return self.bot.session
+  @property
+  def session(self):
+    return self.bot.session
 
-	@discord.utils.cached_property
-	def replied_reference(self):
-		ref = self.message.reference
-		if ref and isinstance(ref.resolved, discord.Message):
-			return ref.resolved.to_reference()
-		return None
+  @discord.utils.cached_property
+  def replied_reference(self):
+    ref = self.message.reference
+    if ref and isinstance(ref.resolved, discord.Message):
+      return ref.resolved.to_reference()
+    return None
 
-	async def disambiguate(self, matches, entry):
-		if len(matches) == 0:
-			raise ValueError('No results found.')
+  async def disambiguate(self, matches, entry):
+    if len(matches) == 0:
+      raise ValueError('No results found.')
 
-		if len(matches) == 1:
-			return matches[0]
+    if len(matches) == 1:
+      return matches[0]
 
-		await self.send(
-		    'There are too many matches... Which one did you mean? **Only say the number**.'
-		)
-		await self.send('\n'.join(f'{index}: {entry(item)}'
-		                          for index, item in enumerate(matches, 1)))
+    await self.send(
+        'There are too many matches... Which one did you mean? **Only say the number**.'
+    )
+    await self.send('\n'.join(f'{index}: {entry(item)}'
+                              for index, item in enumerate(matches, 1)))
 
-		def check(m):
-			return m.content.isdigit(
-			) and m.author.id == self.author.id and m.channel.id == self.channel.id
+    def check(m):
+      return m.content.isdigit(
+      ) and m.author.id == self.author.id and m.channel.id == self.channel.id
 
-		await self.release()
+    await self.release()
 
-		# only give them 3 tries.
-		try:
-			for i in range(3):
-				try:
-					message = await self.bot.wait_for('message',
-					                                  check=check,
-					                                  timeout=30.0)
-				except asyncio.TimeoutError:
-					raise ValueError('Took too long. Goodbye.')
+    # only give them 3 tries.
+    try:
+      for i in range(3):
+        try:
+          message = await self.bot.wait_for('message',
+                                            check=check,
+                                            timeout=30.0)
+        except asyncio.TimeoutError:
+          raise ValueError('Took too long. Goodbye.')
 
-				index = int(message.content)
-				try:
-					return matches[index - 1]
-				except:
-					await self.send(
-					    f'Please give me a valid number. {2 - i} tries remaining...'
-					)
+        index = int(message.content)
+        try:
+          return matches[index - 1]
+        except:
+          await self.send(
+              f'Please give me a valid number. {2 - i} tries remaining...'
+          )
 
-			raise ValueError('Too many tries. Goodbye.')
-		finally:
-			await self.acquire()
+      raise ValueError('Too many tries. Goodbye.')
+    finally:
+      await self.acquire()
 
-	async def prompt(self,
-	                 message,
-	                 *,
-	                 timeout=60.0,
-	                 delete_after=True,
-	                 reacquire=True,
-	                 author_id=None):
-		"""An interactive reaction confirmation dialog.
+  async def prompt(self,
+                   message,
+                   *,
+                   timeout=60.0,
+                   delete_after=True,
+                   reacquire=True,
+                   author_id=None):
+    """An interactive reaction confirmation dialog.
 
         Parameters
         -----------
@@ -133,77 +132,77 @@ class Context(commands.Context):
             ``None`` if deny due to timeout
         """
 
-		if not self.channel.permissions_for(self.me).add_reactions:
-			raise RuntimeError('Bot does not have Add Reactions permission.')
+    if not self.channel.permissions_for(self.me).add_reactions:
+      raise RuntimeError('Bot does not have Add Reactions permission.')
 
-		fmt = f'{message}\n\nReact with \N{WHITE HEAVY CHECK MARK} to confirm or \N{CROSS MARK} to deny.'
+    fmt = f'{message}\n\nReact with \N{WHITE HEAVY CHECK MARK} to confirm or \N{CROSS MARK} to deny.'
 
-		author_id = author_id or self.author.id
-		msg = await self.send(fmt)
+    author_id = author_id or self.author.id
+    msg = await self.send(fmt)
 
-		confirm = None
+    confirm = None
 
-		def check(payload):
-			nonlocal confirm
+    def check(payload):
+      nonlocal confirm
 
-			if payload.message_id != msg.id or payload.user_id != author_id:
-				return False
+      if payload.message_id != msg.id or payload.user_id != author_id:
+        return False
 
-			codepoint = str(payload.emoji)
+      codepoint = str(payload.emoji)
 
-			if codepoint == '\N{WHITE HEAVY CHECK MARK}':
-				confirm = True
-				return True
-			elif codepoint == '\N{CROSS MARK}':
-				confirm = False
-				return True
+      if codepoint == '\N{WHITE HEAVY CHECK MARK}':
+        confirm = True
+        return True
+      elif codepoint == '\N{CROSS MARK}':
+        confirm = False
+        return True
 
-			return False
+      return False
 
-		for emoji in ('\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}'):
-			await msg.add_reaction(emoji)
+    for emoji in ('\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}'):
+      await msg.add_reaction(emoji)
 
-		if reacquire:
-			await self.release()
+    if reacquire:
+      await self.release()
 
-		try:
-			await self.bot.wait_for('raw_reaction_add',
-			                        check=check,
-			                        timeout=timeout)
-		except asyncio.TimeoutError:
-			confirm = None
+    try:
+      await self.bot.wait_for('raw_reaction_add',
+                              check=check,
+                              timeout=timeout)
+    except asyncio.TimeoutError:
+      confirm = None
 
-		try:
-			if reacquire:
-				await self.acquire()
+    try:
+      if reacquire:
+        await self.acquire()
 
-			if delete_after:
-				await msg.delete()
-		finally:
-			return confirm
+      if delete_after:
+        await msg.delete()
+    finally:
+      return confirm
 
-	def tick(self, opt, label=None):
-		lookup = {
-		    True: '<:greenTick:330090705336664065>',
-		    False: '<:redTick:330090723011592193>',
-		    None: '<:greyTick:563231201280917524>',
-		}
-		emoji = lookup.get(opt, '<:redTick:330090723011592193>')
-		if label is not None:
-			return f'{emoji}: {label}'
-		return emoji
+  def tick(self, opt, label=None):
+    lookup = {
+        True: '<:greenTick:330090705336664065>',
+        False: '<:redTick:330090723011592193>',
+        None: '<:greyTick:563231201280917524>',
+    }
+    emoji = lookup.get(opt, '<:redTick:330090723011592193>')
+    if label is not None:
+      return f'{emoji}: {label}'
+    return emoji
 
-	@property
-	def db(self):
-		return self._db if self._db else self.pool
+  @property
+  def db(self):
+    return self._db if self._db else self.pool
 
-	async def _acquire(self, timeout):
-		if self._db is None:
-			self._db = await self.pool.acquire(timeout=timeout)
-		return self._db
+  async def _acquire(self, timeout):
+    if self._db is None:
+      self._db = await self.pool.acquire(timeout=timeout)
+    return self._db
 
-	def acquire(self, *, timeout=300.0):
-		"""Acquires a database connection from the pool. e.g. ::
+  def acquire(self, *, timeout=300.0):
+    """Acquires a database connection from the pool. e.g. ::
 
             async with ctx.acquire():
                 await ctx.db.execute(...)
@@ -216,47 +215,47 @@ class Context(commands.Context):
             finally:
                 await ctx.release()
         """
-		return _ContextDBAcquire(self, timeout)
+    return _ContextDBAcquire(self, timeout)
 
-	async def release(self):
-		"""Releases the database connection from the pool.
+  async def release(self):
+    """Releases the database connection from the pool.
 
         Useful if needed for "long" interactive commands where
         we want to release the connection and re-acquire later.
 
         Otherwise, this is called automatically by the bot.
         """
-		# from source digging asyncpg source, releasing an already
-		# released connection does nothing
+    # from source digging asyncpg source, releasing an already
+    # released connection does nothing
 
-		if self._db is not None:
-			await self.bot.pool.release(self._db)
-			self._db = None
+    if self._db is not None:
+      await self.bot.pool.release(self._db)
+      self._db = None
 
-	async def show_help(self, command=None):
-		"""Shows the help command for the specified command if given.
+  async def show_help(self, command=None):
+    """Shows the help command for the specified command if given.
 
         If no command is given, then it'll show help for the current
         command.
         """
-		cmd = self.bot.get_command('help')
-		command = command or self.command.qualified_name
-		await self.invoke(cmd, command=command)
+    cmd = self.bot.get_command('help')
+    command = command or self.command.qualified_name
+    await self.invoke(cmd, command=command)
 
-	async def safe_send(self, content, *, escape_mentions=True, **kwargs):
-		"""Same as send except with some safe guards.
+  async def safe_send(self, content, *, escape_mentions=True, **kwargs):
+    """Same as send except with some safe guards.
 
         1) If the message is too long then it sends a file with the results instead.
         2) If ``escape_mentions`` is ``True`` then it escapes mentions.
         """
-		if escape_mentions:
-			content = discord.utils.escape_mentions(content)
+    if escape_mentions:
+      content = discord.utils.escape_mentions(content)
 
-		if len(content) > 2000:
-			fp = io.BytesIO(content.encode())
-			kwargs.pop('file', None)
-			return await self.send(file=discord.File(
-			    fp, filename='message_too_long.txt'),
-			                       **kwargs)
-		else:
-			return await self.send(content)
+    if len(content) > 2000:
+      fp = io.BytesIO(content.encode())
+      kwargs.pop('file', None)
+      return await self.send(file=discord.File(
+          fp, filename='message_too_long.txt'),
+                             **kwargs)
+    else:
+      return await self.send(content)
