09102251fd859ca5b61a0abea7041a4a8d3eebb4
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-07 19:41:08 +0530

    Update __init__.py

--- config/languages/__init__.py
+++ config/languages/__init__.py
@@ -1,1 +1,23 @@
-# Soon™
+import os, json
+
+def generate():
+  files = os.listdir("./config/languages/")
+  language_files = ["/config/languages/"+file for file in files if file.endswith(".json")]
+  def _gen(fmt):
+    class _:
+      def __repr__(self): return "lang"
+      def __init__(self):
+        for k,v in fmt.items():
+          if isinstance(v, dict): v = _gen(v)
+          setattr(self, k, v)
+
+    return _()
+
+  languages = {}
+  for file in language_files:
+    languages[file.split("/")[-1][:-5]] = _gen(json.load(open(file,"r")))
+  print(languages)
+  globals().update(languages)
+
+generate()
+del generate()
