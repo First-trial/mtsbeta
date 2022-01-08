cf840ba21c763f8e6ac4caddaa822cb421dcf8e7
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-08 12:50:25 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -2,6 +2,7 @@
 
 import appcommands
 from core import Cog
+from config import languages
 
 
 class Settings(Cog):
@@ -9,8 +10,13 @@ class Settings(Cog):
 
   language = settings.subcommandgroup(name="language", description="Language Settings!")
 
-  @language.sucommand(name="view", description="Get Language of your server")
+  @language.subcommand(name="view", description="Get Language of your server")
   async def settings_language_view(ctx):
-    await ctx.send("Your current guild language is `{}`".format(await ctx.get_lang()))
+    await ctx.send("Your current server language is `{}`".format(await ctx.get_lang()))
 
-#  async def settings_language_edit(ctx, language: str
+  @language.subcommand(name="list", description="Get a list of available languages")
+  async def settings_language_list(ctx):
+    await ctx.send("\n".join(languages.all()))
+
+
+#   async def settings_language_edit(ctx, language: str
