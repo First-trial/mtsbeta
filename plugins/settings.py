f50889cc434ecc1da991160e427b34c8ca5cedad
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-10 12:41:18 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -4,6 +4,7 @@ import discord
 import appcommands
 from core import Cog
 from config import languages
+from models import GLanguage
 
 
 class Settings(Cog):
@@ -23,8 +24,19 @@ class Settings(Cog):
     await ctx.send(embed=discord.Embed("\n".join(list(languages.languages.keys()))))
 
 
-#   async def settings_language_edit(ctx, language: str):
-# Soon™
+  @language.subcommand(name="edit", description="Edit language of current server")
+  async def settings_language_edit(self, ctx, language: str):
+    if not languages.get(language): return await ctx.send(f"Language {language} not found!", ephemeral=True)
+    lang = languages.get(language)
+    language = await ctx.get_language()
+    if not ctx.guild:
+      if await ctx.confirm(f"Are you sure to change language from `{language.__name__}` to `{lang.__name__}`"):
+        await GLanguage.edit(ctx.channel.id, lang.__name__)
+        await ctx.send(f"You have successfully changed the language from `{language.__name__}` to `{lang.__name__}`")
+      return
+
+    await ctx.send("Soon™", ephemeral=True)
+
 
 def setup(bot):
   bot.add_cog(Settings(bot))
