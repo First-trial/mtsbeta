37186fa7e8fce5ce0378165da7a76eebc43eb077
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-11 18:29:30 +0530

    Update maths.py

--- plugins/maths.py
+++ plugins/maths.py
@@ -8,30 +8,31 @@ class MathCog(Cog):
   def __init__(self, bot):
     self.bot = bot
 
-  square = appcommands.slashgroup(name="sqaure",)
+  math = appcommands.slashgroup(name="math",)
+  math_square = math.subcommandgroup(name="sqaure",)
 
-  @appcommands.command(description="Add two numbers")
-  async def add(self, ctx, number: float, into: float):
+  @math.subcommand(description="Add two numbers")
+  async def math_add(self, ctx, number: float, into: float):
     await ctx.reply(f'{number}+{into} = `{number + into}`', ephemeral=True)
     
-  @appcommands.command(description="Subtract two numbers")
-  async def subtract(self, ctx, number: float, by: float):
+  @math.subcommand(description="Subtract two numbers")
+  async def math_subtract(self, ctx, number: float, by: float):
     await ctx.reply(f'{number}-{by} = `{number - by}`', ephemeral=True)
 
-  @appcommands.command(description="Multiply Two numbers")
-  async def multiply(self, ctx, number: float, by: float):
+  @math.subcommand(description="Multiply Two numbers")
+  async def math_multiply(self, ctx, number: float, by: float):
     await ctx.reply(f'{number}x{by} = `{number * by}`', ephemeral=True)
 
-  @appcommands.command(description="Divide two numbers")
-  async def divide(self, ctx, number: float, by: float):
+  @math.subcommand(description="Divide two numbers")
+  async def math_divide(self, ctx, number: float, by: float):
     await ctx.reply(f'{number}/{by} = `{number/by}`', ephemeral=True)
 
-  @square.subcommand(name="of", description="Square of a number")
-  async def square_of(self, ctx, number: float):
+  @math_square.subcommand(name="of", description="Square of a number")
+  async def math_square_of(self, ctx, number: float):
     await ctx.reply(f'{number}*{number} = `{number * number}`', ephemeral=True)
 
-  @square.subcommand(name="root", description="Square root of a number")
-  async def square_root(self, ctx, number: float):
+  @math_square.subcommand(name="root", description="Square root of a number")
+  async def math_square_root(self, ctx, number: float):
     await ctx.reply(f'{number}/{math.sqrt(number)} = `{math.sqrt(number)}`', ephemeral=True)
 
 def setup(bot):
