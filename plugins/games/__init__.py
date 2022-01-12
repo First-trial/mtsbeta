1f6e2626b0eab8942031a4f3373d20ebfaab7eb0
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-12 12:29:13 +0530

    Update __init__.py

--- plugins/games/__init__.py
+++ plugins/games/__init__.py
@@ -2,21 +2,36 @@ import discord
 
 
 class GameButton(discord.ui.Button):
-  def __init__(self,msg,user,*args,**kwargs):
+  def __init__(self,user,**kwargs):
     super().__init__(**kwargs)
-    self.handler = handler
-    self.args = args
+    self.user = user
 
   async def callback(self, interaction):
     await MessageManager.dispatch(interaction, self.emoji)
 
-class MessageManager():
+  async def interaction_check(self, interaction):
+    if interaction.user.id==self.user.id: return True
+    await interaction.response.send_message("You aren't authorised to use this menu!", ephemeral=True)
+    return False
+
+
+class Game(discord.ui.View):
   events = {}
   def __init__(self, message):
+    self.running = False
+    self.players = players
     self.msg = message
+    super().__init__()
 
-  async def add_event(self, emoji, user, handler, *args):
+  def add_event(self, emoji, user, handler, *args):
     self.__class__.events[(self.msg.id, emoji, user)] = (handler, *args)
+
+  def add_button(self, emoji,user):
+    self.add_item(GameButton(user,emoji=emoji))
+
+  def add_button_event(self,emoji,user,handler,*args):
+    self.add_button(emoji,user)
+    self.add_event(emoji,user,handler,*args)
 
   @classmethod
   async def dispatch(cls, payload,emoji):
@@ -24,6 +39,17 @@ class MessageManager():
     if container in cls.events.keys():
       (handler, args) = cls.events[container]
       await handler(*args, payload)
+
+  async def start_game(self):
+    self.running = True
+    for player in self.players: player.play()
+    return self
+
+  async def end_game(self):
+    self.running = False
+    for player in self.players: player.stop()
+    self.stop()
+    return self
     
 
 class Player:
@@ -41,18 +67,6 @@ class Player:
   def stop(self): self.playing = False; return self
   def win(self): self.won,self.lose = True,False; return self
   def lose(self): self.won,self.lose = False, True; return self
-
-class Game:
-  def __init__(self, *players):
-    self.players = players
-
-  async def start_game(self):
-   for player in self.players: player.play()
-   return self
-
-  async def end_game(self):
-   for player in self.players: player.stop()
-    return self
 
 class SinglePlayer(Game): pass
 
