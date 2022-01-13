f1ae75c6af6e2d9726990db41c9d4fe1e383a4c7
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-01-13 11:13:13 +0530

    Update tictactoe.py

--- plugins/games/tictactoe.py
+++ plugins/games/tictactoe.py
@@ -1,4 +1,6 @@
+import copy
 import enum
+import math
 
 from discord import ButtonStyle
 from plugins.games import Player, AiGame, MultiPlayer
@@ -15,7 +17,12 @@ class TicTacToeButton(discord.ui.Button['TicTacToe']):
     view: TicTacToe = self.view
     state = view.board[self.y][self.x]
     cp,c=view.current_player,None
-    gg=view.x if cp==view.X else view.o
+    is_ai_playing = (view.o.ai)
+
+    if not is_ai_playing:
+      gg=view.x if cp==view.X else view.o
+    else: gg = view.x
+
     if f"<@!{interaction.user.id}>" not in (view.x,view.o):
       c="You aren't playing in this match!"
     elif f"<@!{interaction.user.id}>" != gg:
@@ -30,6 +37,8 @@ class TicTacToeButton(discord.ui.Button['TicTacToe']):
       self.label = 'X'
       self.disabled = True
       view.board[self.y][self.x] = view.X
+      if is_ai_playing: view.board[self.y][self.x] = GameState.player
+
       content = "It's {O}\u200b's turn now!"
     else:
       self.style = discord.ButtonStyle.success
@@ -38,7 +47,8 @@ class TicTacToeButton(discord.ui.Button['TicTacToe']):
       view.board[self.y][self.x] = view.O
       content = "It's {X}\u200b's turn now!"
 
-    view.switch_player()
+    if is_ai_playing: view.process_turn()
+    else: view.switch_player()
 
     winner = view.check_board_winner()
     if winner is not None:
@@ -62,29 +72,10 @@ class TicTacToeButton(discord.ui.Button['TicTacToe']):
     )
 
 
-class TicTacToe(MultiPlayer):
-  children: List[TicTacToeButton]
+class TicTacToe_Base:
   X = -1
   O = 1
   Tie = 2
-
-  def __init__(self, msg, XN, ON):
-    x,o = Player(f"<@!{XN}>"), Player(f"<@!{ON}>")
-    super().__init__(msg, x, o)
-    self.current_player = self.X
-
-    self.x = x
-    self.o = o
-
-    self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-    for x in range(3):
-      for y in range(3):
-        self.add_item(TicTacToeButton(x, y))
-
-  def switch_player(self):
-    if self.current_player==self.X: self.current_player=self.O
-    elif self.current_player==self.O: self.current_player=self.X
 
   def check_board_winner(self):
     for across in self.board:
@@ -120,4 +111,117 @@ class TicTacToe(MultiPlayer):
       return self.Tie
 
     return None
+
+  def switch_player(self):
+    if self.current_player==self.X: self.current_player=self.O
+    elif self.current_player==self.O: self.current_player=self.X
+
+
+class TicTacToe(TicTacToe_Base, MultiPlayer):
+  def __init__(self, msg, XN, ON):
+    x,o = Player(f"<@!{XN}>"), Player(f"<@!{ON}>")
+    super().__init__(msg, x, o, timeout=15.0)
+    self.current_player = self.X
+
+    self.x = x
+    self.o = o
+
+    self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    for x in range(3):
+      for y in range(3):
+        self.add_item(TicTacToeButton(x, y))
+
+
+# Ai
+
+class GameState(enum.IntEnum):
+    empty = 0
+    player = -1
+    ai = +1
+
+class TicTacToe_Ai(TicTacToe_Base, AiGame):
+  def __init__(self, msg, player):
+    super().__init__(msg,Player("<@!{player}>"), timeout=15.0)
+    self.x,self.o = self.players
+    self.current_player = self.X
+
+    self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    for x in range(3):
+      for y in range(3):
+        self.add_item(TicTacToeButton(x, y))
+
+  def determine_possible_positions(self, board: list = None):
+    board = board or self.board
+    possible_positions = []
+    for i in range(3):
+      for x in range(3):
+        if board[i][x] == GameState.empty: possible_positions.append([i, x])
+    return possible_positions
+
+  def min_max(self, board: list, depth: int, player: GameState):
+    def determine_win_state(board_, player):
+      win_states = [
+        [board_[0][0], board_[0][1], board_[0][2]],
+        [board_[1][0], board_[1][1], board_[1][2]],
+        [board_[2][0], board_[2][1], board_[2][2]],
+        [board_[0][0], board_[1][0], board_[2][0]],
+        [board_[0][1], board_[1][1], board_[2][1]],
+        [board_[0][2], board_[1][2], board_[2][2]],
+        [board_[0][0], board_[1][1], board_[2][2]],
+        [board_[2][0], board_[1][1], board_[0][2]],
+      ]
+      return [player, player, player] in win_states
+
+    def evaluate(board_):
+      if determine_win_state(board_, GameSate.ai):
+        score = +1
+      elif determine_win_state(board_, GameState.player):
+        score = -1
+      else:
+        score = 0
+
+      return score
+
+    best = [-1, -1, -math.inf]
+    if player == GameState.player: best[-1] = +math.inf
+
+    if (
+      depth == 0
+      or determine_win_state(board, GameState.ai)
+      or determine_win_state(board, GameState.player)
+    ): return [-1, -1, evaluate(board)]
+
+    for cell in self.determine_possible_positions(board):
+      x, y = cell[0], cell[1]
+      board[x][y] = player
+      score = self.min_max(board, depth - 1, -player)
+      board[x][y] = GameState.empty
+      score[0], score[1] = x, y
+      if player == GameState.ai:
+        if score[2] > best[2]:
+          best = score
+      else:
+        if score[2] < best[2]:
+          best = score
+
+    return best
+
+  def process_turn(self,):
+    if self.check_board_winner(): return
+    depth = len(self.determine_possible_positions())
+    if depth == 0: return
+    move = self.min_max(copy.deepcopy(self.board), depth, GameState.ai)
+    row, col = move[0], move[1]
+    self.board[row][col] = self.O
+    _=[[],[],[]]
+
+    for child in self.children:
+      if child.row < 3: _[child.row].append(child)
+
+    button = _[row][col]
+    button.label = "O"
+    button.disabled = True
+    button.style = discord.ButtonStyle.success
 
